fe9e00dd73a995dc5f472a2b70e7aad4de376174
Russell <75209707+rustyff12@users.noreply.github.com>
2025-06-23 14:53:58 +0200

    import type change

--- src/app/api/notes/[id]/edit/route.ts
+++ src/app/api/notes/[id]/edit/route.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import sanitizeHtml from "sanitize-html";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
 
   const body = await req.json();
   const sanitizedTitle = sanitizeHtml(body.title);
